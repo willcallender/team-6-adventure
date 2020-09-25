aac6ed22e7dd49306a54a944837da98059480e82
William Callender <29266431+willcallender@users.noreply.github.com>
2020-09-25 10:47:46 -0400

    Added basic player character
    Also organized the assets folder

---
+++ gdf20_team6_adventure/Assets/Player/playerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript : MonoBehaviour {
+    public float speed;
+    float x, y;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start() {
+        rb = GetComponent<Rigidbody2D> ();
+    }
+    
+    // Update is called once per frame
+    void Update() {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+        rb.velocity = new Vector3(x * speed, y * speed, 0f);
+    }
+}
