f34842c8ad4d5f4c74db0e1ec67c23f76effb756
William Callender <29266431+willcallender@users.noreply.github.com>
2020-10-13 12:52:36 -0400

    Tried to make a loading screen, doesn't work
    It loads the next scene before displaying the loading scene, which defeats the purpose of the loading screen

---
+++ gdf20_team6_adventure/Assets/Scripts/loadingSpinner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loadingSpinner : MonoBehaviour
+{
+    public GameObject spinner;
+
+    public GameObject sceneControl;
+
+    void Start() {
+        print("start");
+        StartCoroutine(sceneControl.GetComponent<sceneController>().changeSceneAsync("Level 1"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spinner.transform.Rotate(0,0,-1);
+    }
+}
---
+++ gdf20_team6_adventure/Assets/Scripts/sceneController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneController : MonoBehaviour
+{
+    public void changeScene(string name) {
+        SceneManager.LoadScene(name);
+    }
+
+    public IEnumerator changeSceneAsync(string name) {
+        AsyncOperation sceneLoad = SceneManager.LoadSceneAsync(name);
+
+        while (sceneLoad.progress < 1) {
+            print(sceneLoad.progress);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
