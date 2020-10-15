1e603400f403006281a377fb79b624e00a43c05b
William Callender <29266431+willcallender@users.noreply.github.com>
2020-10-15 12:28:58 -0400

    Update animation control script
    Animation transitions now happen instantaneously

--- gdf20_team6_adventure/Assets/Scripts/playerMovement.cs
+++ gdf20_team6_adventure/Assets/Scripts/playerMovement.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class playerMovement : MonoBehaviour {
+    // speed multiplier
     public float speed;
+    // the x and y component of user input
     float x, y;
+    // the x and y component of translation
+    float tx, ty;
+    // the values of x and y from the previous frame
+    float px, py = 0;
+    // the text box object to write to
     public GameObject textBox;
+    // the player animation component to control
     Animator anim;
 
     // Start is called before the first frame update
@@ -17,19 +25,59 @@ public class playerMovement : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         // get inputs
-        x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-        y = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        x = Input.GetAxisRaw("Horizontal");
+        y = Input.GetAxisRaw("Vertical");
         
-        transform.Translate(x, y, 0);
+        // this needs to happen because the delta time may change with each frame, making the animation conditions unhelpful
+        tx = x * speed * Time.deltaTime;
+        ty = y * speed * Time.deltaTime;
 
-        // update anim variables
-        anim.SetFloat("x", x);
-        anim.SetFloat("y", y);
+        transform.Translate(tx, ty, 0);
 
-        if (x == 0 && y == 0) {
-            anim.SetBool("isIdle", true);
-        } else {
-            anim.SetBool("isIdle", false);
+        updateAnim();
+        px = x;
+        py = y;
+    }
+
+    void updateAnim() { 
+        // only update if other axis not already doing something (if running up, don't change to sideways on press left and hold up)
+        // if only x changed and y is 0
+        if (px != x && py == y && y == 0) {
+            if (x == 0) {
+                anim.Play("idle");
+            } else if (x > 0) {
+                anim.Play("elf_run_right");
+            } else {
+                anim.Play("elf_run_left");
+            }
+        }
+        // if only y changed and x is 0
+        else if (py != y && px == x && x == 0) {
+            if (y == 0) {
+                anim.Play("idle");
+            } else if (y > 0) {
+                anim.Play("elf_run_up");
+            } else {
+                anim.Play("elf_run_down");
+            }
+        } 
+        // if both changed
+        else if (px != x && py != y) {
+            if (x == 0 && y == 0) {
+                anim.Play("idle");
+            } else if (x == 0) {
+                if (y > 0) {
+                    anim.Play("elf_run_up");
+                } else {
+                    anim.Play("elf_run_down");
+                }
+            } else {
+                if (x > 0) {
+                    anim.Play("elf_run_right");
+                } else {
+                    anim.Play("elf_run_left");
+                }
+            }
         }
     }
 
