6a5f569aa71d10c053da9cd5ac9122b0c4c665ac
willcallender <29266431+willcallender@users.noreply.github.com>
2020-10-20 12:10:27 -0400

    Fixed play errors, but...
    For some reason it doesn't finish building and I had to force close unity after 7 minutes of building

--- gdf20_team6_adventure/Assets/PSDImporterOverrideEditor.cs
+++
@@ -1,8 +0,0 @@
-namespace UnityEditor.U2D.PSD
-{
-    [CustomEditor(typeof(UnityEditor.U2D.PSD.PSDImporterOverride))]
-    internal class PSDImporterOverrideEditor : PSDImporterEditor
-    {
-    }
-
-}
