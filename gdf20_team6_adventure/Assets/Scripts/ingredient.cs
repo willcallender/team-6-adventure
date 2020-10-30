3c8f676544847bc46d9cd1e56ae896d2621087af
willcallender <29266431+willcallender@users.noreply.github.com>
2020-10-29 22:35:21 -0400

    Upload current WIP to website

--- gdf20_team6_adventure/Assets/Scripts/ingredient.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ingredient : MonoBehaviour {
-    // declare int to store what type of ingredient this is
-    public int ID;
-}
