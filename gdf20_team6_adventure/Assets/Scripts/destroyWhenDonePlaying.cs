cc5bd5b8645c392e8be5d26e3cf64c593d263f84
willcallender <29266431+willcallender@users.noreply.github.com>
2020-12-08 17:17:14 -0500

    Explosion sound object now destroys self when done playing

---
+++ gdf20_team6_adventure/Assets/Scripts/destroyWhenDonePlaying.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyWhenDonePlaying : MonoBehaviour
+{
+    AudioSource audioSourceComponent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSourceComponent = GetComponent<AudioSource>();
+        StartCoroutine(waitForAudioFinish());
+    }
+
+    IEnumerator waitForAudioFinish() {
+        yield return new WaitUntil(isDone);
+        Destroy(gameObject);
+    }
+
+    bool isDone() {
+        return !audioSourceComponent.isPlaying;
+    }
+}
