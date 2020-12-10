77406db041c92d52545fe061eb73c4d5f6f8eea5
William Callender <29266431+willcallender@users.noreply.github.com>
2020-12-10 13:34:48 -0500

    Finishing touches

---
+++ gdf20_team6_adventure/Assets/returnToMainMenu.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class returnToMainMenu : MonoBehaviour
+{
+    public void returnToMenu() {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
